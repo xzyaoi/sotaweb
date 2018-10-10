cc41454d4eb90406ebcc71ac6cee2a80aaa8899a
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2018-10-10 23:01:55 +0800

    add a logo & small style fixes

--- static/main.js
+++ static/main.js
@@ -8,9 +8,9 @@ var current_keyword = ""
  * @param {*} start 
  */
 
-function fetchPapers(keyword, max,start) {
+function fetchPapers(keyword, max, start) {
     var url = new URL("https://sotaresearch.pythonanywhere.com"),
-    params = {keyword:keyword, max:max, start:start}
+        params = { keyword: keyword, max: max, start: start }
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
     fetch(url).then(function (res) {
         return res.json()
@@ -22,18 +22,18 @@ function fetchPapers(keyword, max,start) {
     })
 }
 
-function renderPapers (papers) {
+function renderPapers(papers) {
     document.getElementById("results").innerHTML = tmpl("tmpl-results", papers)
 }
 
-function searchArxiv (self) {
+function searchArxiv(self) {
     var keyword = self.value
     current_curr = 0
     current_keyword = keyword
     fetchPapers(keyword, 5, 0)
 }
 
-function prev () {
+function prev() {
     if (current_curr - 5 < 0) {
         return;
     } else {
@@ -41,16 +41,18 @@ function prev () {
     }
 }
 
-function next () {
+function next() {
     fetchPapers(current_keyword, 5, current_curr)
 }
 
 /** Control Page DOMs */
-function hideHint () {
+function hideHint() {
     document.getElementById('hint').style.display = "none"
 }
 
-function triggerSideNav () {
+function triggerSideNav() {
     var instance = M.Sidenav.getInstance('sidenav-trigger')
     console.log(instance)
 }
+
+/** User Management */
